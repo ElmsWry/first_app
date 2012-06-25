8a414c3fe5340eee93f4ad2d04ad0408d4ae3d2d
Elms Wry <ixoxi@kortisin.com>
2012-06-25 02:36:22 +0200

    Not much has changed, beautified a bit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpasTest::Application.config.secret_token = '4727b0402d1f210bcd30fea7acbe5dcfe0cf864f262b9d2ab6d5eb6a53ecf23f18112ee86bd9a6bc5754c5bfe5086b70122130a6fcaec9d24c13136ac6ce352d'
